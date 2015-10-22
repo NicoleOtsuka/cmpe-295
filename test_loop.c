d71f9137d23293ef699e5c6df195e2d87769ee54
Nicolin Chen <nicoleotsuka@gmail.com>
2015-10-22 16:46:07 -0700

    Reduce the delays for IP testing
    Signed-off-by: Nicolin Chen <nicoleotsuka@gmail.com>

--- test_loop.c
+++ test_loop.c
@@ -150,7 +150,7 @@ int main()
 	reg_write(regmap, 0x0, 0x1f);
 	reg_write(regmap, 0x0, 0x0);
 
-	reg_write(regmap, 0x1c, 130000);
+	reg_write(regmap, 0x1c, 1000);
 
 	dma_init(&ipif.dma[0], &dma_config[0]);
 	dma_init(&ipif.dma[2], &dma_config[2]);
